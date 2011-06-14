433813085efa8cd28b1f5c3d2674c721f909a5e3
J.P. Cummins <jpcummins@gmail.com>
2011-06-13 21:30:05 -0700

    Removed the Backbone

--- public/js/collections.js
+++
@@ -1,9 +0,0 @@
-confab.collections = {};
-
-confab.collections.MessageCollection = Backbone.Collection.extend({
-  model: confab.models.Message,
-
-  comparator: function(message) {
-    return message.get('timestamp');
-  }
-});
